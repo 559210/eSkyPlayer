d86627c0611126aebc163dcb1cb840a90a95551b
lizheng <molika202@163.com>
2018-02-01 17:51:56 +0800

    恢复文件编码格式

--- eSkyPlayerCameraPlanTrackData.lua
+++ eSkyPlayerCameraPlanTrackData.lua
@@ -2,7 +2,7 @@ local prototype = class("eSkyPlayerCameraPlanTrackData", require("eSkyPlayer/eSk
 local definations = require("eSkyPlayer/eSkyPlayerDefinations");
 
 function prototype:ctor()
-    prototype.super.ctor(self);       --由于多重继承，只能用prototype.super这种写法
+    prototype.super.ctor(self);       --鐢变簬澶氶噸缁ф壙锛屽彧鑳界敤prototype.super杩欑鍐欐硶
     self.trackType_ = definations.TRACK_TYPE.CAMERA_PLAN;
     self.trackFileType_ = definations.TRACK_FILE_TYPE.CAMERA;
     self.eventsSupportted_ = {definations.EVENT_TYPE.CAMERA_PLAN};
