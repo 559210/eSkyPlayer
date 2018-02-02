556cc8c36844e611da2cd0eab67b8670248c64e1
Abby5 <chaonan.song@xiangyounet.com>
2018-02-02 18:31:16 +0800

    增加CameraMotion播放时镜头曲线的功能

--- eSkyPlayerCameraMotionPlayer.lua
+++ eSkyPlayerCameraMotionPlayer.lua
@@ -60,7 +60,6 @@ function prototype:_update()
         if self.director_.timeLine_ >= beginTime and self.director_.timeLine_ <= endTime then
             local isEnterEvent = true;
             if #self.cameraJobsQueue_ > 0 then 
-
                 for j = 1, #self.cameraJobsQueue_ do
                     local queue = self.cameraJobsQueue_[j];
                     if queue.event == event then
@@ -111,9 +110,16 @@ end
 function prototype:_transformCamera(queue)
     local camera = queue.camera;
     local event = queue.event;
-    local deltaTime = (self.director_.timeLine_ - queue.beginTime) / event.eventData_.timeLength ;
-    camera.transform.position = Vector3.Lerp (event.eventData_.beginFrame, event.eventData_.endFrame, deltaTime );
-    camera.transform.rotation = Quaternion.Lerp (event.eventData_.beginDr, event.eventData_.endDr, deltaTime );
+    local deltaTime = self.director_.timeLine_ - queue.beginTime;
+    if event.eventData_.tweenType == 0 then
+        deltaTime = self:_getTime(deltaTime, event.eventData_.timeLength, event.eventData_.pos1, event.eventData_.pos2);
+        if deltaTime > event.eventData_.timeLength then
+            deltaTime = event.eventData_.timeLength;
+        end
+    end
+    local ratio = deltaTime / event.eventData_.timeLength ;
+    camera.transform.position = Vector3.Lerp (event.eventData_.beginFrame, event.eventData_.endFrame, ratio );
+    camera.transform.rotation = Quaternion.Lerp (event.eventData_.beginDr, event.eventData_.endDr, ratio );
 end
 
 function prototype:_requestCamera()
@@ -155,5 +161,70 @@ function prototype:_isNeedAdditionalCamera()
         self.isNeedAdditionalCamera_ = false;
     end
 end
+
+
+function prototype:_getTime(time, totalTime, ctrlPoint1, ctrlPoint2)
+    if time < 0 then
+        time = 0;
+    end
+    if totalTime < 0 then
+        totalTime = 0;
+    end
+    local timeRatio = Mathf.Clamp(time / totalTime, 0, 1);
+    if time > totalTime then
+        timeRatio = 1;
+    end
+    if timeRatio == 0 or timeRatio == 1 then
+        return time;
+    end
+    local curveResultData = {};
+    curveResultData.pos = Vector2.New(x,y);
+    curveResultData.minTime = 0;
+    curveResultData.maxTime = 1;
+    while (true) do
+        curveResultData = self:_getNearPoint(timeRatio, curveResultData.minTime, curveResultData.maxTime, ctrlPoint1, ctrlPoint2);
+        local value = curveResultData.pos.y - timeRatio;
+        if value < 0.0001 and value >= 0 then
+            break;
+        end
+    end
+    return curveResultData.pos.x * totalTime;
+end
+
+function prototype:_getNearPoint(posY, minTimeRatio, maxTimeRatio, ctrlPoint1, ctrlPoint2)
+    local _curveResultData = {};
+    local middleTimeRatio = self:_getMiddleTimeRatio(minTimeRatio, maxTimeRatio);
+    _curveResultData.pos = self:_getPosByTime(middleTimeRatio, self:_getPoints(ctrlPoint1, ctrlPoint2, Rect.New(0, 0, 1, 1)));
+    _curveResultData.minTime = middleTimeRatio;
+    _curveResultData.maxTime = maxTimeRatio;
+    if posY < _curveResultData.pos.y then
+        _curveResultData.minTime = minTimeRatio;
+        _curveResultData.maxTime = middleTimeRatio;
+    end
+
+    return _curveResultData;
+end
+
+function prototype:_getMiddleTimeRatio(minTimeRatio, maxTimeRatio)
+    local middleTimeRatio = (maxTimeRatio - minTimeRatio) * 0.5 + minTimeRatio;
+    return middleTimeRatio;
+end
+
+function prototype:_getPoints(ctrlPoint1, ctrlPoint2, rect)
+    local points = {};
+    points[0] = Vector2.New(rect.x, rect.y);
+    points[3] = Vector2.New(rect.width, rect.height);
+    points[1] = Vector2.New(rect.width * ctrlPoint1.x, rect.height * ctrlPoint1.y);
+    points[2] = Vector2.New(rect.width * ctrlPoint2.x, rect.height * ctrlPoint2.y); 
+    return points;
+end
+
+function prototype:_getPosByTime(t, points)
+    local p = Vector2.New(x,y);
+    p.x = points[0].x * (1 - t) ^ 3 + 3 * points[1].x * t * (1 - t) ^ 2 + 3 * points[2].x * t ^ 2 * (1 - t) + points[3].x * t ^ 3;
+    p.y = points[0].y * (1 - t) ^ 3 + 3 * points[1].y * t * (1 - t) ^ 2 + 3 * points[2].y * t ^ 2 * (1 - t) + points[3].y * t ^ 3;
+    return p;
+end
+
 
 return prototype;
